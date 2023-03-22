c5ab2e89aab6058ffba82ed01dd9e42122e6b6cc
Deity2001 <98880618+Deity2001@users.noreply.github.com>
2023-03-22 01:15:44 -0700

    Delete sample.js

--- sample.js
+++
@@ -1,9 +0,0 @@
-let hello = "manoj";
-let no1 = 1;
-let no2 = 2;
-let x="is an enterprenure";
-
-let addition = no1+no2;
-let mk = hello +x+ addition;
-console.log( mk );
-console.log("I need to say something about"  + x + "at vellore");  
